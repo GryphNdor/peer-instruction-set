ab5e9e69dcee52673451cba84f2e2ff888e8ed92
GryphNdor <griffin@ogura-family.com>
2024-11-05 13:42:14 -0500

    mantine

---
+++ src/app/nav.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { Group, UnstyledButton, rem } from '@mantine/core'
+import { IconHome, IconZoom, IconUser, IconListCheck, IconDeviceTv } from '@tabler/icons-react'
+import React from 'react'
+import { useRouter, usePathname } from 'next/navigation'
+
+export default function Nav() {
+  const router = useRouter()
+  const currentPath = usePathname()
+
+  const checkPath = (path: string): string => {
+    if (currentPath === path) {
+      return "black"
+    }
+    return "grey"
+  }
+
+  return (
+    <Group justify='space-between'>
+      <UnstyledButton onClick={() => router.push('/')}>
+        <IconHome style={{ width: rem(30), height: rem(30), color: checkPath("/") }} />
+      </UnstyledButton>
+      <UnstyledButton onClick={() => router.push('/search')}>
+        <IconZoom style={{ width: rem(30), height: rem(30), color: checkPath("/search") }} />
+      </UnstyledButton>
+      <UnstyledButton>
+        <IconUser style={{ width: rem(30), height: rem(30), color: checkPath("/user") }} />
+      </UnstyledButton>
+      <UnstyledButton>
+        <IconListCheck style={{ width: rem(30), height: rem(30), color: checkPath("/user") }} />
+      </UnstyledButton>
+      <UnstyledButton>
+        <IconDeviceTv style={{ width: rem(30), height: rem(30), color: checkPath("/user") }} />
+      </UnstyledButton>
+    </Group>
+  )
+}
